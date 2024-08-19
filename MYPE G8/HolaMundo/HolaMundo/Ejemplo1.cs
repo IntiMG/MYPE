56f148621b675fef95a251ea00b39a8825a291b9
IntiMG <intimontalvan@uamv.edu.ni>
2024-08-19 10:49:02 -0600

    Agrego la carpeta de los proyectos

---
+++ MYPE G8/HolaMundo/HolaMundo/Ejemplo1.cs
@@ -0,0 +1,34 @@
+﻿namespace HolaMundo
+{
+    internal class Ejemplo1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inti Montalvan");
+            //Tipos de datos primitvos
+            int edad = 17;
+            Console.WriteLine($"Tu edad es: {edad}");
+            string nombre = "Inti Montalvan";
+            Console.WriteLine($"Tu nombre es: {nombre}");
+            Console.WriteLine($"Tu nombre es: {nombre.ToUpper()}");
+            Console.WriteLine($"Tu nombre es: {nombre.ToLower()}");
+
+            //Length permite saber el numero de caracteres de la variable
+            Console.WriteLine($"Cauntas letras tiene tu nombre: {nombre.Length} letras");
+
+            //Variables de doble precision
+            double precio = 12.99;
+            Console.WriteLine($"El precio es de: {precio}");
+            Console.WriteLine($"El precio es de: {precio++}");
+            Console.WriteLine($"El precio es de: {++precio}");
+
+            /* Valores Logicos */
+            bool isSingle = true;
+            Console.WriteLine($"Tu eres soltero: {isSingle}");
+            Console.WriteLine($"Tu eres soltero: {!isSingle}");
+            Console.WriteLine($"Tu eres soltero: {isSingle}");
+
+        }
+    }
+}
